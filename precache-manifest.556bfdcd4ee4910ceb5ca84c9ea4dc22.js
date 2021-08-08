db462e344475652b412674ee93940e1917dd1c45
Simon <simon.zabrocki@gmail.com>
2021-08-08 17:59:49 +0900

    Updates

---
+++ precache-manifest.556bfdcd4ee4910ceb5ca84c9ea4dc22.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9579b52be5809e93bf37d3e517d6b50",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "9ab858d55ed27af3df50",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "385bc3e61d3effd2a5d4",
+    "url": "/home/static/js/2.2374709b.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.2374709b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ab858d55ed27af3df50",
+    "url": "/home/static/js/main.637d9f1b.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "9f9631d008daf59a1829419286419b63",
+    "url": "/home/static/media/GGGI.9f9631d0.png"
+  },
+  {
+    "revision": "ef313694df86687cd6be3e2f882d9a45",
+    "url": "/home/static/media/ZABROCKI_CV.ef313694.pdf"
+  },
+  {
+    "revision": "d58c4d353db0c7fc8f3eddb6be4c1045",
+    "url": "/home/static/media/dashboard.d58c4d35.png"
+  },
+  {
+    "revision": "51d02ac2354b07f4f9c5ee52a97c44fe",
+    "url": "/home/static/media/hawadawa.51d02ac2.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  },
+  {
+    "revision": "6b607c65fd5e29433d6dc90fc5a2cd1a",
+    "url": "/home/static/media/sanofi.6b607c65.png"
+  },
+  {
+    "revision": "7ba449b24a89ed426a3759dbfd6eb0e1",
+    "url": "/home/static/media/simonzabrocki.7ba449b2.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4e623e3fe0bb348596bf0e23920b28ff.js"
+  "/home/precache-manifest.556bfdcd4ee4910ceb5ca84c9ea4dc22.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
